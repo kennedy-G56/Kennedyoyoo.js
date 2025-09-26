63a493737c8b583b1464b96ca2bcfa83bdb4a85e
kennedy-G56 <oyookenny7019@gmail.com>
2025-09-26 11:18:13 +0300

    Update Kennedyoyoo.js

--- Kennedyoyoo.js
+++ Kennedyoyoo.js
@@ -6,9 +6,21 @@ function isOdd(number) {
   return number % 2 !== 0; // or Math.abs(number % 2) === 1; for negative numbers
 }
 
-// Test cases
 console.log(isEven(4)); 
 console.log(isOdd(7));  
 console.log(isEven(-2));
 console.log(isOdd(-3));
 console.log(isOdd(2));
+
+
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString("kennedy"));
+console.log(reverseString("magical"));
+console.log(reverseString("eclipse"));
+console.log(reverseString("987654321"));
+                         
+                   
